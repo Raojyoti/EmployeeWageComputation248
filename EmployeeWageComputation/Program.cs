809676130b47af2458544722b094cccdbbda22f5
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-01-20 15:21:01 +0530

    [Jyoti Rao] Add Project of EmployeeWageComputation

---
+++ EmployeeWageComputation/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeeWageComputation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Employee Wage Computation");
+            Console.ReadLine();
+        }
+    }
+}
